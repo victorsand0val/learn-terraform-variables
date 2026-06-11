23d93c2e2be207da369e7513691dd148a1703334
victorsand0val <victorsando.jr2006@gmail.com>
2026-06-11 11:42:08 -0400

    Change environment tag from dev to development

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "vpc" {
 
   tags = {
     project     = "project-alpha",
-    environment = "dev"
+    environment = "development"
   }
 }
 
@@ -54,7 +54,7 @@ module "app_security_group" {
 
   tags = {
     project     = "project-alpha",
-    environment = "dev"
+    environment = "development"
   }
 }
 
@@ -70,7 +70,7 @@ module "lb_security_group" {
 
   tags = {
     project     = "project-alpha",
-    environment = "dev"
+    environment = "development"
   }
 }
 
@@ -111,7 +111,7 @@ module "elb_http" {
 
   tags = {
     project     = "project-alpha",
-    environment = "dev"
+    environment = "development"
   }
 }
 
@@ -125,6 +125,6 @@ module "ec2_instances" {
 
   tags = {
     project     = "project-alpha",
-    environment = "dev"
+    environment = "development"
   }
 }
